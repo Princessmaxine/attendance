3018fa0582c7b4451f5a4fc08c50614ad0122f06
Princessmaxine <94942829+Princessmaxine@users.noreply.github.com>
2023-11-14 00:14:05 -0500

    Initial commit

---
+++ db/crud.php
@@ -0,0 +1,113 @@
+<?php
+class crud{
+    private $db;
+
+    function __construct($conn){
+        $this->db = $conn;
+}
+public function insertAttendees($fname, $lname, $dob, $email, $contact, $specialty){
+    try {
+        $sql = "INSERT INTO attendee (firstname,lastname,dateofbirth,emailaddress,contactnumber,specialty_id ) 
+        VALUES(:fname, :lname, :dob, :email, :contact, :specialty)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":fname", $fname); 
+        $stmt->bindParam(":lname", $lname);
+        $stmt->bindParam(":dob", $dob);
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":contact", $contact);
+        $stmt->bindParam(":specialty", $specialty);
+        $stmt->execute();
+        return true;
+
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+
+    }
+                 
+}
+
+public function editAttendee($id, $fname, $lname, $dob, $email, $contact, $specialty){
+    try {
+        $sql = "UPDATE `attendee` SET `firstname`=:fname, `lastname`=:lname,`dateofbirth`=:dob,
+`emailaddress`=:email,`contactnumber`=:contact,`specialty_id`=:specialty WHERE attendee_id =:id ";
+$stmt = $this->db->prepare($sql);
+$stmt->bindParam(":id", $id);
+$stmt->bindParam(":fname", $fname); 
+$stmt->bindParam(":lname", $lname);
+$stmt->bindParam(":dob" ,$dob);
+$stmt->bindParam(":email", $email);
+$stmt->bindParam(":contact", $contact);
+$stmt->bindParam(":specialty", $specialty);
+$stmt->execute();
+return true;
+ }catch (PDOException $e) {
+    echo $e->getMessage();
+    return false;
+
+
+}
+}
+
+public function getAttendees(){
+    try{
+        $sql = "SELECT * FROM `attendee`  a inner join specialties s on a.specialty_id = s.specialty_id";
+        $result = $this->db->query($sql);
+        return $result; 
+
+    }catch(PDOException $e) {
+        echo $e->getMessage();
+        return false;
+    }
+}
+
+public function getAttendeeDetails($id){
+    try {
+        $sql = "SELECT * FROM `attendee`  a inner join  specialties s on a.specialty_id = s.specialty_id 
+        where attendee_id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(' :id', $id);
+         $stmt->execute();
+         $result = $stmt->fetch();
+        return $result;
+
+    }catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+   
+}
+}
+
+public function deleteAttendee($id) {
+    try {
+        $sql = "delete from attendee where attendee_id = :id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindparam(':id', $id);
+        $stmt->execute();
+        return true;
+
+    }catch (PDOException $e) {
+        echo $e->getMessage();
+        return false;
+}
+
+}
+
+public function getSpecialties(){
+   try{
+    $sql = "SELECT * FROM `specialties`";
+    $result = $this->db->query($sql);
+    return $result;
+
+}catch (PDOException $e) {
+    echo $e->getMessage();
+    return false;
+
+    } 
+    
+    }
+
+
+}
+?>
---
+++ view.php
@@ -0,0 +1,57 @@
+<?php 
+$title = 'View Record';
+
+require_once 'includes/header.php';
+require_once 'db/conn.php';
+
+//get attendee by id 
+if(!isset($_GET['id'])){
+    echo "<h1>Please check details and try again</h1>";
+    $id = $_GET['id'];
+    $result = $crud->getAttendeeDetails($id) ;
+
+} else{    $id = $_GET['id'];
+    $result = $crud->getAttendeeDetails($id);
+   
+
+
+?>
+
+<div class="card" style="width: 18rem;">
+   <div class="card-body">
+
+    <h5 class="card-title">
+    <?php echo $result['firstname'] . ' ' . $result['lastname'];  ?> 
+  </h5>
+
+    <h6 class="card-subtitle mb-2 text-muted">
+        <?php echo $result['name']; ?> 
+</h6>
+
+    <p class="card-text">
+    Date of Birth: <?php echo $result['dateofbirth']; ?> 
+</p>
+
+    <p class="card-text">
+    Email Address: <?php echo $result['emailaddress']; ?> 
+</p>   
+
+    <p class="card-text">
+   Contact Number: <?php echo $result['contactnumber']; ?> 
+</p>
+     
+  </div>
+</div>
+<br/>
+    <a href="viewrecords.php" class="btn btn-info">Back to the List</a>
+    <a href="edit.php?id=<?php echo $result['attendee_id'] ?>" class="btn btn-warning">Back to the List</a>
+    <a onclick="return confirm('Are you sure you want to delete this record?');"
+    href="delete.php?id=<?php echo $result['attendee_id'] ?>" class="btn btn-danger">Delete</a>
+        
+<?php } ?>
+
+<br>
+<br>
+<br>
+<br>
+<?php require_once 'includes/footer.php';?>  
